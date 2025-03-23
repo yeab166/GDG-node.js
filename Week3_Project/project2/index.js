bd59d0d8ab6ea1ed858e9cc8320867566f048977
Yeabsira Belete <yeababa16@gmail.com>
2025-03-23 17:14:14 +0300

    Update index.js

--- Week3_Project/project2/index.js
+++ Week3_Project/project2/index.js
@@ -1,1 +1,11 @@
-a
+const lodash= require('lodash');
+
+const numbers=[35,75,45,95,72,64];
+
+const maxNumber= lodash.max(numbers);
+
+const minNumber= lodash.min(numbers);
+
+console.log("the maxNumber is: ", maxNumber);
+
+console.log("The minNumber is: ", minNumber);
